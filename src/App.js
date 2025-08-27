a3b34d54f17fb615704c4dd411cca7efb7d9a713
AbdullahM4 <abdullahm.alyousef10@gmail.com>
2025-08-27 14:35:48 +0300

    Update React frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import './App.scss';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import Teams from './components/Teams';
+import TeamData from './components/TeamData';
+import Nation from "./components/Nation";
+import Position from "./components/Position";
+import Search from "./components/Search";
 
 function App() {
+  useEffect(() => {
+    document.title = 'PremierPoints Fantasy';
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="teams" element={<Teams />} />
+          <Route path="data" element={<TeamData />} />
+          <Route path="nation" element={<Nation />} />
+          <Route path="position" element={<Position />} />
+          <Route path="search" element={<Search />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Home/index.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import Loader from 'react-loaders';
+import { Link } from 'react-router-dom';
+import LogoPL from '../../assets/images/PL.png';
+import AnimatedLetters from '../AnimatedLetters';
+import './index.scss';
+
+const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = "Welcome to".split("");
+    const jobArray = "Premier Points Fantasy!".split("");
+
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+          setLetterClass('text-animate-hover');
+        }, 4000);
+      
+        return () => {
+          clearTimeout(timerId);
+        };
+      }, []);
+
+    return(
+      <>
+        <div className = "container home-page">
+            <div className="text-zone">
+                <h1>
+                <img src={LogoPL} alt = "PremierPoints" />
+                <br />
+                <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={12} />
+                <br /> 
+                <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={15} /> 
+                </h1>
+                <h2>Your home for everything Premier League related to help you getting top rank in fantasy!</h2>
+                <br/>
+                <Link to="/teams" className="flat-button">GET STARTED</Link>
+            </div>
+        </div>
+        <Loader type="pacman" />
+      </>
+    )
+}
+
+export default Home
